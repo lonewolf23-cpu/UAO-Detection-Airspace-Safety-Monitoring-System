17d1f1bd93cc362f7b1d96630e913dbbc5e3213b
lone_wolf_FX <whitefang203y@gmail.com>
2026-02-19 10:10:31 +0530

    Enhance object detection with new categories and logic
    Expanded the object detection list to include birds and flocks, and updated the return logic to classify birds as non-threats.

--- src/detection.py
+++ src/detection.py
@@ -1,6 +1,25 @@
 import random
 
 def detect_object():
-    objects = ["Drone", "Aircraft", "Unknown Object","Unidentified Object","Drones", "Aircrafts", "Unknown Objects","Unidentified Objects" ]
+    
+    objects = [
+        "Drone",
+        "Aircraft",
+        "Unknown Object",
+        "Unidentified Object",
+        "Bird",
+        "Flock of Birds",
+        "Drones",
+        "Aircrafts",
+        "Unknown Objects",
+        "Unidentified Objects",
+        "Birds"
+    ]
+
     detected = random.choice(objects)
-    return detected
+
+    if detected == "Bird" or detected == "Flock of Birds":
+        return detected, "NON-THREAT"
+    
+    else:
+        return detected
