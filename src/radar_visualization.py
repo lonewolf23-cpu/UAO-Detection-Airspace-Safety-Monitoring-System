bbbe6270376149bdb558b73628cbe889115849c6
lone_wolf_FX <whitefang203y@gmail.com>
2026-02-21 14:06:53 +0530

    Update radar_visualization.py

--- src/radar_visualization.py
+++ src/radar_visualization.py
@@ -1,31 +1,43 @@
+import matplotlib
+matplotlib.use("Agg")
+
 import matplotlib.pyplot as plt
+import os
 
-def show_radar(current_position, next_position, restricted_zone_center, radius):
 
-    plt.clf()
+def show_radar(current_position, next_position, restricted_zone_center, radius, step):
 
-    fig = plt.gcf()
-    ax = fig.add_subplot(111)
+    if not os.path.exists("radar_frames"):
+        os.makedirs("radar_frames")
 
-    ax.set_xlim(-200, 200)
-    ax.set_ylim(-200, 200)
+    plt.figure(figsize=(6, 6))
 
-    radar = plt.Circle(
+    plt.xlim(-200, 200)
+    plt.ylim(-200, 200)
+
+    # Restricted airspace
+    circle = plt.Circle(
         (restricted_zone_center[0], restricted_zone_center[1]),
         radius,
         fill=False
     )
+    plt.gca().add_patch(circle)
 
-    ax.add_patch(radar)
+    # UAV current & predicted position
+    plt.scatter(current_position[0], current_position[1], label="Current Position")
+    plt.scatter(next_position[0], next_position[1], marker="x", label="Predicted Position")
 
-    ax.scatter(current_position[0], current_position[1], marker='o')
-    ax.scatter(next_position[0], next_position[1], marker='x')
-
-    ax.plot(
+    # Path line
+    plt.plot(
         [current_position[0], next_position[0]],
         [current_position[1], next_position[1]]
     )
 
     plt.title("Airspace Radar Monitoring")
+    plt.xlabel("X Coordinate")
+    plt.ylabel("Y Coordinate")
+    plt.legend()
 
-    plt.pause(0.1)
+    filename = f"radar_frames/frame_{step}.png"
+    plt.savefig(filename)
+    plt.close()
